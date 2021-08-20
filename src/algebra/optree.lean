6c72769e08eae412a09a41e327aaf2717c6ba734
Junology <jtachan@hotmail.com>
2021-08-21 01:00:20 +0900

    Algebraic theories in terms of operator trees.

---
+++ src/algebra/optree.lean
@@ -0,0 +1,460 @@
+import data.vect.basic
+import tactic.csimp
+
+universes u v
+
+-- Tree of operators
+inductive optree (op : ℕ → Type u) (α : Type v) : Type.{max u v}
+| varleaf {} (a : α) : optree
+| opnode {n : ℕ} (f : op n) : vect optree n → optree
+
+namespace optree
+
+/-**********************************-
+ - Well-founded recursion on optree
+ -**********************************-/
+
+-- Check if the second argument is a parent of the first
+definition is_child {op : ℕ → Type*} {α : Type*} : optree op α → optree op α → Prop
+| t (varleaf _) := false
+| t (opnode f sub) := sub.mem t
+
+-- Count the number of nods
+mutual definition nnodes, nnodes_vec
+with nnodes : Π{op : ℕ → Type*}, Π{α : Type*}, optree op α → ℕ
+| _ _ (varleaf _) := 0
+| _ _ (opnode f vect.nil) := 1
+| _ _ (opnode f (vect.cons x xs)) := nnodes x + nnodes_vec xs + 1
+with nnodes_vec : Π{op : ℕ → Type*}, Π {α : Type*}, Π {n : ℕ}, vect (optree op α) n → ℕ
+| _ _ _ vect.nil := 0
+| _ _ _ (vect.cons x xs) := nnodes x + nnodes_vec xs
+
+-- Well-founded recursion based on the number of nodes
+definition nnodes_wf {op : ℕ → Type*} {α : Type*} :=
+  measure_wf (@nnodes op α)
+
+-- Auxiliary function to define nnodes_child
+theorem nnodes_vec_mem {op : ℕ → Type*} {α : Type*} (tm : optree op α) {n : ℕ} (ts : vect (optree op α) n) : ts.mem tm → nnodes tm ≤ nnodes_vec ts:=
+  begin
+    induction ts with n ts_head ts_tail h_ind,
+    case vect.nil {
+      exact false.elim
+    },
+    case vect.cons {
+      simp [vect.mem,nnodes_vec],
+      intro hmem,
+      cases hmem,
+      case or.inl {
+        rw [hmem],
+        exact nat.le_add_right tm.nnodes (nnodes_vec ts_tail)
+      },
+      case or.inr {
+        by calc
+          tm.nnodes
+              ≤ nnodes_vec ts_tail : h_ind hmem
+          ... ≤ ts_head.nnodes + nnodes_vec ts_tail : nat.le_add_left _ _
+      }
+    }
+  end
+
+-- If a tree is a child of another, then the number of nodes decreases
+theorem nnodes_child {op : ℕ → Type*} {α : Type*} (tc tp : optree op α) : is_child tc tp → nnodes tc < nnodes tp :=
+  begin
+    intros hchild,
+    cases tp with _ n f ts,
+    case optree.varleaf { exact false.elim hchild },
+    unfold is_child at hchild,
+    cases ts with n t ts,
+    case vect.nil {
+      exact false.elim hchild
+    },
+    case vect.cons {
+      unfold nnodes,
+      unfold vect.mem at hchild,
+      cases hchild,
+      case or.inl {
+        rw [hchild],
+        by calc
+          tc.nnodes
+              ≤ tc.nnodes + nnodes_vec ts : nat.le_add_right _ _
+          ... < tc.nnodes + nnodes_vec ts + 1
+                : nat.lt_add_of_pos_right (nat.succ_pos 0)
+      },
+      case or.inr {
+        by calc
+          tc.nnodes
+              ≤ nnodes_vec ts : nnodes_vec_mem tc ts hchild
+          ... ≤ t.nnodes + nnodes_vec ts : nat.le_add_left _ _
+          ... < t.nnodes + nnodes_vec ts + 1
+                : nat.lt_add_of_pos_right (nat.succ_pos 0)
+      }
+    }
+  end
+
+-- Accessibility for optree
+theorem accessible {op : ℕ → Type*} {α : Type*}: ∀ {t : optree op α}, acc is_child t :=
+  begin
+    intro t,
+    apply nnodes_wf.fix,
+    intros x h_ind,
+    clear t,
+    simp [measure, inv_image] at h_ind,
+    apply acc.intro,
+    intros y hchild,
+    exact h_ind y (nnodes_child y x hchild)
+  end
+
+-- Well-founded recursion based on the parent-children relation.
+definition children_wf {op : ℕ → Type*} {α : Type*} : well_founded (@is_child op α) :=
+  well_founded.intro (@accessible op α)
+
+
+/-************************-
+ - Basic operations
+ -************************-/
+-- Mapping on variables
+mutual definition map , map_aux {op : ℕ → Type*} {α : Type*} {β : Type*} (f : α → β)
+with map : optree op α → optree op β
+| (varleaf a) := varleaf (f a)
+| (opnode g vect.nil) := opnode g vect.nil
+| (opnode g (vect.cons x xs)) := opnode g (vect.cons (map x) (map_aux xs))
+with map_aux : Π{n : ℕ}, vect (optree op α) n → vect (optree op β) n
+| _ vect.nil := vect.nil
+| _ (vect.cons x xs) := vect.cons (map x) (map_aux xs)
+
+-- Join 2-fold trees
+mutual definition join, join_aux {op : ℕ → Type*} {α : Type*}
+with join : optree op (optree op α) → optree op α
+| (varleaf t) := t
+| (opnode g vect.nil) := opnode g vect.nil
+| (opnode g (vect.cons x xs)) := opnode g (vect.cons (join x) (join_aux xs))
+with join_aux : Π{n : ℕ}, vect (optree op (optree op α)) n → vect (optree op α) n
+| _ vect.nil := vect.nil
+| _ (vect.cons t ts) := vect.cons (join t) (join_aux ts)
+
+-- Eliminator
+mutual definition elim, elim_aux {op : ℕ → Type*} {α : Type*} {β : Type*} (act : Π {n : ℕ}, op n → vect β n → β) (c : α → β)
+with elim : optree op α → β
+| (varleaf a) := c a
+| (opnode f vect.nil) := act f vect.nil
+| (opnode f (vect.cons t ts)) := act f (vect.cons (elim t) (elim_aux ts))
+with elim_aux : Π{n : ℕ}, vect (optree op α) n → vect β n
+| _ vect.nil := vect.nil
+| _ (vect.cons t ts) := vect.cons (elim t) (elim_aux ts)
+
+-- Unzipping pairs of variables into the same shape of trees
+/-
+definition unzip {op : ℕ → Type.{u}} {α : Type.{v}} {β : Type.{w}} : optree op (α×β) → optree op α × optree op β :=
+  λ t, t.elim
+    (λ _ f xs, (opnode f xs.unzip.fst, opnode f xs.unzip.snd))
+    (λ (x : α×β), (optree.varleaf x.fst, optree.varleaf x.snd))
+-/
+mutual definition unzip, unzip_aux {op : ℕ → Type*} {α : Type*} {β : Type*}
+with unzip : optree op (α×β) → optree op α × optree op β
+| (varleaf t) := (varleaf t.fst, varleaf t.snd)
+| (opnode f vect.nil) := (opnode f vect.nil, opnode f vect.nil)
+| (opnode f (vect.cons t ts)) := (opnode f (vect.cons (unzip t).fst (unzip_aux ts).fst), opnode f (vect.cons (unzip t).snd (unzip_aux ts).snd))
+with unzip_aux : Π{n : ℕ}, vect (optree op (α×β)) n → vect (optree op α) n × vect (optree op β) n
+| _ vect.nil := (vect.nil, vect.nil)
+| _ (vect.cons t ts) := (vect.cons (unzip t).fst (unzip_aux ts).fst, vect.cons (unzip t).snd (unzip_aux ts).snd)
+
+
+/-***********************-
+ - Basic properties
+ -***********************-/
+-- "Non-homogeneous" injectivity of opnode
+theorem opnode_inj_nh {op : ℕ → Type*} {α : Type*} : Π {n : ℕ} {f1 f2 : op n} {t1 t2 : vect (optree op α) n}, opnode f1 t1 = opnode f2 t2 → f1=f2 ∧ t1=t2 :=
+  begin
+    intros n f1 f2 t1 t2 h,
+    cases (opnode.inj h).right.left,
+    cases (opnode.inj h).right.right,
+    split; refl
+  end
+
+-- Differenti constructors produce different terms
+theorem varleaf_neq_opnode {op : ℕ → Type*} {α : Type*} : ∀ {a : α} {n : ℕ} {f : op n} {t : vect (optree op α) n}, varleaf a ≠ opnode f t :=
+  begin
+    intros,
+    simp *
+  end
+
+-- Functoriality
+mutual theorem map_comp, map_aux_comp {op : ℕ → Type*} {α : Type*} {β : Type*} {γ : Type*} {g : β → γ} {f : α → β}
+with map_comp : ∀ (t : optree op α), map (g∘f) t = map g (map f t)
+| (varleaf _) := by unfold map; refl
+| (opnode _ vect.nil) := by unfold map
+| (opnode _ (vect.cons t ts)) :=
+  begin
+    unfold map,
+    rw [map_comp t, map_aux_comp ts]
+  end
+with map_aux_comp : ∀ {n : ℕ} (ts : vect (optree op α) n), map_aux (g∘f) ts = map_aux g (map_aux f ts)
+| _ vect.nil := by unfold map_aux; refl
+| _ (vect.cons t ts) :=
+  begin
+    unfold map_aux,
+    rw [map_comp t, map_aux_comp ts]
+  end
+
+-- Left unitalities of join; i.e. eT = id
+theorem join_unit_outer {op : ℕ → Type*} {α : Type*} : ∀ {t : optree op α}, join (varleaf t) = t :=
+  λ t, by unfold join
+
+-- Right unitality of join; i.e. Te = id
+mutual theorem join_unit_inner, join_aux_unit_inner {op : ℕ → Type*} {α : Type*}
+with join_unit_inner : ∀ {t : optree op α}, join (map optree.varleaf t) = t
+| (varleaf a) := by unfold map; unfold join
+| (opnode f vect.nil) := by unfold map; unfold join
+| (opnode f (vect.cons t ts)) :=
+  begin
+    simp [map,join],
+    rw[join_unit_inner, join_aux_unit_inner],
+    try {split; refl}
+  end
+with join_aux_unit_inner : ∀ {n : ℕ} {ts : vect (optree op α) n}, join_aux (map_aux optree.varleaf ts) = ts
+| _ vect.nil := by simp [map_aux,join_aux]; refl
+| _ (vect.cons t ts) :=
+  begin
+    simp [map_aux,join_aux],
+    split,
+    show (map varleaf t).join = t,
+      from join_unit_inner,
+    show join_aux (map_aux varleaf ts) = ts,
+      from join_aux_unit_inner
+  end
+
+-- Associativity of join
+mutual theorem join_assoc, join_aux_assoc {op : ℕ → Type*} {α : Type*}
+with join_assoc : ∀ {t : optree op (optree op (optree op α))}, join (join t) = join (map join t)
+| (varleaf _) := by simp [map, join]
+| (opnode _ vect.nil) := by simp [map, join]; try {split; refl}
+| (opnode _ (vect.cons t ts)) :=
+  begin
+    simp [map, join],
+    rw [join_assoc, join_aux_assoc],
+    try {split; refl}
+  end
+with join_aux_assoc : ∀ {n : ℕ} {ts : vect (optree op (optree op (optree op α))) n}, join_aux (join_aux ts) = join_aux (map_aux join ts)
+| _ vect.nil := by simp [map_aux, join_aux]; refl
+| _ (vect.cons t ts) :=
+  begin
+    simp [map_aux, join_aux],
+    split,
+    show join (join t) = join (map join t),
+      from join_assoc,
+    show join_aux (join_aux ts) = join_aux (map_aux join ts),
+      from join_aux_assoc
+  end
+
+-- elim_aux is equivalent to map elim
+theorem elim_aux_map {op : ℕ → Type*} {α β : Type*} {act : Π {n : ℕ}, op n → vect β n → β} {c : α → β} : ∀ {n : ℕ} {ts : vect (optree op α) n}, elim_aux @act c ts = vect.map (elim @act c) ts
+| _ vect.nil := by unfold elim_aux; unfold vect.map
+| _ (vect.cons t ts) :=
+  begin
+    unfold elim_aux; unfold vect.map,
+    rw [elim_aux_map]
+  end
+
+#print axioms elim_aux_map
+
+-- The eliminator at varleaf
+theorem elim_varleaf {op : ℕ → Type*} {α β : Type*} {act : Π {n : ℕ}, op n → vect β n → β} {c : α → β} : ∀ {a : α}, elim @act c (varleaf a) = c a :=
+  by intros; unfold elim; refl
+
+-- The eliminator at opnode
+theorem elim_opnode {op : ℕ → Type*} {α : Type*} {β : Type*} {act : Π {n : ℕ}, op n → vect β n → β} {c : α → β} : ∀ {n : ℕ} {f : op n} {ts : vect (optree op α) n}, elim @act c (opnode f ts) = act f (vect.map (elim @act c) ts)
+| _ f vect.nil := by unfold elim; unfold vect.map
+| _ f (vect.cons t ts) :=
+  begin
+    unfold elim; unfold vect.map,
+    rw [elim_aux_map]
+  end
+
+#print axioms elim_opnode
+
+-- The eliminator respects function extensionality
+mutual theorem elim_funext, elim_aux_funext {op : ℕ → Type*} {α : Type*} {β : Type*} {act₁ act₂ : Π {n : ℕ}, op n → vect β n → β} {c₁ c₂ : α → β} (hact : ∀ {n : ℕ} {f : op n} {bs : vect β n}, act₁ f bs = act₂ f bs) (hc : ∀ {a : α}, c₁ a = c₂ a)
+with elim_funext : ∀ {t : optree op α}, elim @act₁ c₁ t = elim @act₂ c₂ t
+| (varleaf x) := by simp [elim, hc]
+| (opnode f vect.nil) := by simp [elim,hact]
+| (opnode f (vect.cons t ts)) :=
+  by simp [elim]; rw [elim_funext,elim_aux_funext, hact]
+with elim_aux_funext : ∀ {n : ℕ} {ts : vect (optree op α) n}, elim_aux @act₁ c₁ ts = elim_aux @act₂ c₂ ts
+| _ vect.nil := by simp [elim_aux]; refl
+| _ (vect.cons t ts) :=
+  begin
+    simp [elim_aux],
+    rw [elim_funext,elim_aux_funext],
+    split; refl
+  end
+
+-- Elimination into pairs produces pairs of elimination
+mutual theorem elim_prod, elim_aux_prod {op : ℕ → Type*} {α : Type*} {β₁ : Type _} {β₂ : Type _} {act₁ : Π {n : ℕ}, op n → vect β₁ n → β₁} {act₂ : Π {n : ℕ}, op n → vect β₂ n → β₂} (c : α → β₁×β₂)
+with elim_prod : ∀ {t : optree op α}, t.elim (λ _ f, prod.map (act₁ f) (act₂ f) ∘ vect.unzip) c = (t.elim @act₁ (prod.fst∘c), t.elim @act₂ (prod.snd∘ c))
+| (varleaf x) := by unfold elim; unfold function.comp; rw [prod.mk.eta]
+| (opnode f vect.nil) := by unfold elim; unfold function.comp; unfold vect.unzip; unfold prod.map
+| (opnode f (vect.cons t ts)) :=
+  begin
+    csimp [elim, vect.unzip, prod.map],
+    rw [elim_prod, elim_aux_prod]
+  end
+with elim_aux_prod : ∀ {n : ℕ} {ts : vect (optree op α) n}, (@elim_aux op α (β₁×β₂) (λ _ f, prod.map (act₁ f) (act₂ f) ∘ vect.unzip) c n ts).unzip = (@elim_aux op α β₁ @act₁ (prod.fst∘c) n ts,@elim_aux op α β₂ @act₂ (prod.snd∘ c) n ts)
+| _ vect.nil := by csimp [elim_aux, vect.unzip]
+| _ (vect.cons t ts) :=
+  begin
+    csimp [elim_aux, vect.unzip, prod.map],
+    rw [elim_prod, elim_aux_prod]
+  end
+
+#print axioms elim_prod
+
+-- theorem elim_ext {op : ℕ → Type.{u}} {α : Type.{v}} {β : Type _} {B : β → Type _} {act : Π {n : ℕ}, op n → vect (Π b, B b) n → Π b, B b} {c : α → Π b, B b} := sorry
+
+-- Elimination into pi-types produces pi of elimination
+mutual theorem elim_pi, elim_aux_pi {op : ℕ → Type*} {α : Type*} {β : Type _} {B : β → Type _} {act : Π (b : β) {n : ℕ}, op n → vect (B b) n → B b} {c : Π b, α → B b}
+with elim_pi : ∀ {t : optree op α} {b : β}, t.elim (λ k f ts, λ (b : β), @act b k f (ts.unzip_fam b)) (λ a b, c b a) b = t.elim (@act b) (c b)
+| (varleaf x) b := by unfold elim
+| (opnode f vect.nil) b := by unfold elim; unfold vect.unzip_fam
+| (opnode f (vect.cons t ts)) b :=
+  by unfold elim; unfold vect.unzip_fam; rw [elim_pi, elim_aux_pi]
+with elim_aux_pi : ∀ {n : ℕ} {ts : vect (optree op α) n} {b : β}, (elim_aux (λ k f ts, λ (b : β), @act b k f (ts.unzip_fam b)) (λ a b, c b a) ts).unzip_fam b = elim_aux (@act b) (c b) ts
+| _ vect.nil b := by unfold elim_aux; refl
+| _ (vect.cons t ts) b :=
+  begin
+    unfold elim_aux; unfold vect.unzip_fam,
+    apply congr; try {apply congr}; try {refl},
+    rw [elim_pi],
+    rw [elim_aux_pi]
+  end
+
+#print axioms elim_pi
+
+mutual theorem elim_subtype, elim_aux_subtype {op : ℕ → Type*} {α : Type*} {β : Type _} {P : β → Prop} {act : Π {n : ℕ}, op n → vect β n → β} {hsub : ∀ {n : ℕ} (f : op n) (ts : vect (subtype P) n), P (act f (vect.map subtype.val ts))} {c : α → subtype P}
+with elim_subtype : ∀ {t : optree op α}, subtype.val (elim (λ n k (rs : vect (subtype P) n), ⟨@act n k (vect.map subtype.val rs), hsub k rs⟩) c t) = elim @act (subtype.val ∘ c) t
+| (varleaf x) := by unfold elim
+| (opnode f vect.nil) := by unfold elim; unfold vect.map
+| (opnode f (vect.cons t ts)) :=
+  begin
+    unfold elim; unfold vect.map; unfold subtype.val,
+    rw [elim_subtype, elim_aux_subtype]
+  end
+with elim_aux_subtype : ∀ {n : ℕ} {ts : vect (optree op α) n}, vect.map subtype.val (elim_aux (λ n k (rs : vect (subtype P) n), ⟨@act n k (vect.map subtype.val rs), hsub k rs⟩) c ts) = elim_aux @act (subtype.val ∘ c) ts
+| _ vect.nil := by unfold elim_aux; refl
+| _ (vect.cons t ts) :=
+  begin
+    unfold elim_aux; unfold vect.map,
+    rw [elim_subtype, elim_aux_subtype],
+    try {split; refl}
+  end
+
+#print axioms elim_subtype
+
+-- unzip is an injective function.
+mutual theorem unzip_inj, unzip_aux_inj {op : ℕ → Type*} {α : Type*} {β : Type*}
+with unzip_inj : ∀ {t1 t2 : optree op (α×β)}, t1.unzip = t2.unzip → t1 = t2
+| (varleaf x) (varleaf y) heq :=
+  begin
+    suffices : x=y, by rw [this],
+    cases x,
+    cases y,
+    unfold unzip at heq,
+    unfold prod.fst at heq; unfold prod.snd at heq,
+    let h := prod.mk.inj heq,
+    rw [varleaf.inj h.left, varleaf.inj h.right]
+  end
+| (varleaf x) (opnode g vect.nil) heq :=
+  begin
+    unfold unzip at heq,
+    have : varleaf x.fst = opnode g vect.nil,
+      from (prod.mk.inj heq).left,
+    cases this
+  end
+| (varleaf x) (opnode g (vect.cons y ys)) heq :=
+  begin
+    unfold unzip at heq,
+    have : varleaf x.fst = opnode g _,
+      from (prod.mk.inj heq).left,
+    cases this
+  end
+| (opnode f vect.nil) (varleaf y) heq :=
+  begin
+    unfold unzip at heq,
+    have : opnode f _ = varleaf y.fst,
+      from (prod.mk.inj heq).left,
+    cases this
+  end
+| (opnode f (vect.cons x xs)) (varleaf y) heq :=
+  begin
+    unfold unzip at heq,
+    have : opnode f _ = varleaf y.fst,
+      from (prod.mk.inj heq).left,
+    cases this
+  end
+| (opnode f vect.nil) (opnode g vect.nil) heq :=
+  begin
+    unfold unzip at heq,
+    have : f = g,
+      from (opnode_inj_nh (prod.mk.inj heq).left).left,
+    rw [this]
+  end
+| (opnode f vect.nil) (opnode g (vect.cons y ys)) heq :=
+  by unfold unzip at heq; cases heq
+| (opnode f (vect.cons x xs)) (opnode g vect.nil) heq :=
+  by unfold unzip at heq; cases heq
+| (opnode f (vect.cons x xs)) (opnode g (vect.cons y ys)) heq :=
+  begin
+    unfold unzip at heq,
+    cases (opnode.inj ((prod.mk.inj heq).left)).left,
+    cases (opnode_inj_nh ((prod.mk.inj heq).left)).left,
+    have h_head : x = y, {
+      apply unzip_inj,
+      cases x.unzip with x_fst x_snd,
+      cases y.unzip with y_fst y_snd,
+      have h_fst : x_fst = y_fst,
+        from (vect.cons.inj ((opnode_inj_nh ((prod.mk.inj heq).left)).right)).left,
+      have h_snd : x_snd = y_snd,
+        from (vect.cons.inj ((opnode_inj_nh ((prod.mk.inj heq).right)).right)).left,
+      rw [h_fst,h_snd]
+    },
+    have h_tail : xs = ys, {
+      apply unzip_aux_inj,
+      cases unzip_aux xs with xs_fst xs_snd,
+      cases unzip_aux ys with ys_fst ys_snd,
+      have h_fst : xs_fst = ys_fst,
+        from (vect.cons.inj ((opnode_inj_nh ((prod.mk.inj heq).left)).right)).right,
+      have h_snd : xs_snd = ys_snd,
+        from (vect.cons.inj ((opnode_inj_nh ((prod.mk.inj heq).right)).right)).right,
+      rw [h_fst,h_snd]
+    },
+    rw [h_head, h_tail]
+  end
+with unzip_aux_inj : ∀{n : ℕ} {ts1 ts2 : vect (optree op (α×β)) n}, unzip_aux ts1 = unzip_aux ts2 → ts1 = ts2
+| _ vect.nil vect.nil heq := by trivial
+| _ (vect.cons t1 ts1) (vect.cons t2 ts2) heq :=
+  begin
+    unfold unzip_aux at heq,
+    let h1 := vect.cons.inj ((prod.mk.inj heq).left),
+    let h2 := vect.cons.inj ((prod.mk.inj heq).right),
+    have ht : t1 = t2, {
+      apply unzip_inj,
+      cases t1.unzip with t1_fst t1_snd,
+      cases t2.unzip with t2_fst t2_snd,
+      unfold prod.fst at h1 h2,
+      unfold prod.snd at h1 h2,
+      rw [h1.left,h2.left]
+    },
+    have hts : ts1 = ts2, {
+      apply unzip_aux_inj,
+      cases unzip_aux ts1 with ts1_fst ts1_snd,
+      cases unzip_aux ts2 with ts2_fst ts2_snd,
+      dsimp [prod.fst,prod.snd] at h1 h2,
+      rw [h1.right,h2.right]
+    },
+    rw [ht,hts]
+  end
+
+#print axioms optree.unzip_inj
+
+end optree
+
+
---
+++ src/algebra/theory.lean
@@ -0,0 +1,239 @@
+import algebra.optree
+import data.vect.basic
+import data.finite.finord
+
+universes u v
+
+-- Structure for algebraic theories
+structure theory :=
+  mk :: (op : ℕ → Type) (rel : ℕ → Type)
+        (rel_lhs : ∀ {n : ℕ}, rel n → optree op (finord n))
+        (rel_rhs : ∀ {n : ℕ}, rel n → optree op (finord n))
+
+
+-- Pre-model of a theory; i.e. sets with structure maps which not necessarily satisfy the axioms.
+class premodel (th : theory) (α : Type u) : Type u :=
+  mk :: (act : Π {n : ℕ}, th.op n → vect α n → α)
+
+-- Model of a theory
+class model (th : theory) (α : Type u) extends premodel th α : Type u:=
+  mk :: (haxiom : ∀ {n : ℕ} (r : th.rel n) (var : finord n → α), (th.rel_lhs r).elim @act var = (th.rel_rhs r).elim @act var)
+
+namespace premodel
+
+definition action (th : theory) (α : Type _) [premodel th α] : Π {n : ℕ}, th.op n → vect α n → α := @premodel.act th α _
+
+-- Product of two premodels
+definition prod {th : theory} (α β : Type _) [premodel th α] [premodel th β] : premodel th (α×β):=
+  {
+    act :=
+      λ (n : ℕ) (f : th.op n), prod.map (premodel.act f) (premodel.act f) ∘ vect.unzip
+  }
+
+attribute [instance] premodel.prod
+
+-- Product of families of premodels
+definition pi {th : theory} {α : Type*} (M : α → Type _) [∀ a, premodel th (M a)] : premodel th (Π a, M a) :=
+  {
+    act :=
+      λ n f xs, λ a,
+        premodel.act f (xs.unzip_fam a)
+  }
+
+attribute [instance] premodel.pi
+
+-- optree
+instance tree_model (th : theory) (α : Type*) : premodel th (optree th.op α) :=
+  {
+    act := @optree.opnode th.op α
+  }
+
+end premodel
+
+namespace model
+
+definition axiom_eq (th : theory) (α : Type _) [model th α] : ∀ {n : ℕ} (r : th.rel n) (var : finord n → α), (th.rel_lhs r).elim (@premodel.act th α _) var = (th.rel_rhs r).elim (@premodel.act th α _) var := @model.haxiom th α _
+
+-- Product of two models is a model
+definition prod {th : theory} (α β : Type _) [model th α] [model th β] : model th (α×β) :=
+  {
+    haxiom :=
+      begin
+        intros,
+        unfold premodel.act,
+        rw [optree.elim_prod,optree.elim_prod],
+        rw [axiom_eq,axiom_eq]
+      end
+  }
+
+#print axioms model.prod
+
+-- Product of families of models is a model
+-- WARNING: This requires function extensionality
+definition pi {th : theory} {α : Type _} {C : α → Type _} [∀ a, model th (C a)] : model th (Π a, C a):=
+  {
+    haxiom :=
+      begin
+        intros,
+        apply funext,
+        intro a,
+        unfold premodel.act at *; dsimp *,
+        let dact : Π (a : α) {k : ℕ}, th.op k → vect (C a) k → (C a) := λ a k f rs , premodel.act f rs,
+        let dvar : Π (a : α), finord n → (C a) := λ k a, var a k,
+        rw [@optree.elim_pi th.op _ α C dact dvar _ a],
+        rw [@optree.elim_pi th.op _ α C dact dvar _ a],
+        rw [axiom_eq]
+      end
+  }
+
+#print axioms model.pi
+
+end model
+
+
+/-*****************************
+ - Morphisms of pre-models
+ -*****************************-/
+
+structure morphism (th : theory) (α : Type u) (β : Type v) [premodel th α] [premodel th β] : Type.{max u v+1}:=
+  mk :: (to_fun : α → β) (hact : ∀ {n : ℕ} (f : th.op n) (as : vect α n), to_fun (@premodel.act th α _ _ f as) = (@premodel.act th β _ _ f (as.map to_fun)))
+
+namespace morphism
+
+protected
+definition coe_to_fun {th : theory} (α : Type _) (β : Type _) [premodel th α] [premodel th β] : has_coe_to_fun (morphism th α β)  :=
+  {
+    F := λ_, α → β,
+    coe := morphism.to_fun
+  }
+
+attribute [instance] morphism.coe_to_fun
+
+-- the identity morphism
+definition id {th : theory} {α : Type _} [premodel th α] : morphism th α α :=
+  {
+    to_fun := id,
+    hact := by intros; rw [vect.map_id]; refl
+  }
+
+-- the composition of morphisms
+definition comp {th : theory} {α β γ : Type _} [premodel th α] [premodel th β] [premodel th γ] : morphism th β γ → morphism th α β → morphism th α γ :=
+  λ g f,
+    {
+      to_fun := g.to_fun ∘ f.to_fun,
+      hact :=
+        begin
+          intros n k cs,
+          unfold function.comp,
+          rw [vect.map_comp],
+          rw [f.hact,g.hact]
+        end
+    }
+
+#print axioms morphism.comp
+
+-- maps into premodels give rise to morphisms out of trees
+definition treelift (th : theory) {α β: Type*} [premodel th β] (f : α → β) : morphism th (optree th.op α) β :=
+  {
+    to_fun := optree.elim (@premodel.act th β _) f,
+    hact :=
+      begin
+        intros n k ts,
+        unfold premodel.act,
+        rw [optree.elim_opnode]
+      end
+  }
+
+#print axioms treelift
+
+-- treelift preserves the original map
+theorem treelift_comp {th : theory} {α β: Type*} [premodel th β] (f : α → β) : ∀ {a : α}, (treelift th f).to_fun (optree.varleaf a) = f a :=
+  begin
+    intros,
+    dsimp [treelift],
+    exact optree.elim_varleaf
+  end
+
+#print axioms treelift_comp
+
+-- The embedding into a treemodel is "epimorphic"
+mutual theorem tree_unique, tree_unique_aux {th : theory} {α β : Type*} [premodel th β] {f g : morphism th (optree th.op α) β} (h : ∀ a, f.to_fun (optree.varleaf a) = g.to_fun (optree.varleaf a))
+with tree_unique : ∀ {t : optree th.op α}, f.to_fun t = g.to_fun t
+| (optree.varleaf a) := h a
+| (optree.opnode k vect.nil) :=
+  begin
+    have : (optree.opnode k vect.nil) = (@premodel.act th (optree th.op α) _ 0 k) vect.nil,
+      by unfold premodel.act; refl,
+    rw [this],
+    rw [f.hact,g.hact],
+    unfold vect.map
+  end
+| (optree.opnode k (vect.cons t ts)) :=
+  begin
+    have : ∀ t, (optree.opnode k t) = (@premodel.act th (optree th.op α) _ _ k) t,
+      from λ_, rfl,
+    rw [this],
+    rw [f.hact,g.hact],
+    unfold vect.map,
+    rw [tree_unique, tree_unique_aux]
+  end
+with tree_unique_aux : ∀ {n : ℕ} {ts : vect (optree th.op α) n}, vect.map f.to_fun ts = vect.map g.to_fun ts
+| _ vect.nil := by unfold vect.map; refl
+| _ (vect.cons t ts) :=
+  begin
+    unfold vect.map,
+    rw [tree_unique, tree_unique_aux],
+    try {split; refl}
+  end
+
+#print axioms tree_unique
+
+-- Two morphisms equal to each other as soon as their underlying maps do
+theorem morphism_eq {th : theory} {α β : Type _} [premodel th α] [premodel th β] {f g : morphism th α β} : f.to_fun = g.to_fun → f = g :=
+  begin
+    intros h,
+    cases f,
+    cases g,
+    unfold morphism.to_fun at h,
+    cases h,
+    refl
+  end
+
+#print axioms morphism_eq
+
+-- The image of every morphism is closed under operations
+theorem image_act {th : theory} {α β : Type _} [premodel th α] [premodel th β] (f : morphism th α β) : ∀ {n : ℕ} {k : th.op n} {ys : vect {b // ∃ a, f.to_fun a = b} n}, ∃ a, f.to_fun a = (@premodel.act th β _ _ k) (vect.map subtype.val ys) :=
+  begin
+    intros,
+    apply exists.elim (vect.image ys),
+    intros as has,
+    existsi (@premodel.act th α _ _ k) as,
+    rw [f.hact, has]
+  end
+
+#print axioms image_act
+
+-- The image of a morphism forms a premodel
+definition image_premodel {th : theory} {α β : Type _} [premodel th α] [premodel th β] (f : morphism th α β) : premodel th {b // ∃ a, f.to_fun a = b} :=
+  {
+    act :=
+      λ n k xs, ⟨@premodel.act th β _ _ k (vect.map subtype.val xs), image_act f⟩
+  }
+
+-- The image of a morphism in a model forms a model
+definition image_model {th : theory} {α β : Type _} [premodel th α] [model th β] (f : morphism th α β) : model th {b // ∃ a, f.to_fun a = b} :=
+  {
+    to_premodel := image_premodel f,
+    haxiom :=
+      begin
+        intros,
+        dsimp [image_premodel] at *,
+        apply subtype.eq,
+        rw [optree.elim_subtype,optree.elim_subtype],
+        rw [model.axiom_eq]
+      end
+  }
+
+#print axioms image_model
+
+end morphism

e73d3e2e0082d32c1863b916f6ba4686a5db58cf
Junology <jtachan@hotmail.com>
2021-09-08 12:45:08 +0900

    Proof of the fact that the product of binary_modules is simultaneously the coproduct.

--- src/algebra/binary_module/hom.lean
+++ src/algebra/binary_module/hom.lean
@@ -63,11 +63,20 @@ definition inv_is_morphism (α β : Type _) [model binary_module α] [model bina
 #print axioms inv_is_morphism
 
 --- The hom-set between binary_modules forms a binary_module.
-@[reducible]
+@[reducible,inline]
 definition hom (α β : Type _) [model binary_module α] [model binary_module β] := submodel binary_module (inv_is_morphism α β)
 
+namespace hom
+
+--- Composition of homomorphisms.
+@[reducible,inline]
+protected
+definition comp {α β γ: Type _} [model binary_module α] [model binary_module β] [model binary_module γ] : hom β γ → hom α β → hom α γ := morphism.comp
+
 --- Evaluation at `a : α` defines a homomorphism from `hom α β` to `β`.
-definition hom_eval (α β : Type _) [model binary_module α] [model binary_module β] (a : α) : morphism binary_module (hom α β) β :=
+@[reducible]
+protected
+definition eval (α β : Type _) [model binary_module α] [model binary_module β] (a : α) : morphism binary_module (hom α β) β :=
 {
   val := λ f, f.val a,
   property :=
@@ -83,31 +92,65 @@ definition hom_eval (α β : Type _) [model binary_module α] [model binary_modu
     end
 }
 
-#print axioms hom_eval
+#print axioms hom.eval
 
---- For every pair of `binary_module`s `α` and `β`, the type of homomorphisms `hom α β` is a model of `binary_module` up to `funext`.
-definition hom_model_funext (α β : Type _) [model binary_module α] [model binary_module β] : ∀ {n : ℕ} (r : binary_module.rel n) (var : finord n → hom α β) (a : α), ((binary_module.rel_lhs r).elim (@premodel.act binary_module (hom α β) _) var).val a = ((binary_module.rel_rhs r).elim (@premodel.act binary_module (hom α β) _) var).val a :=
+--- For every pair of `binary_module`s `α` and `β`, the type of homomorphisms `hom α β` is point-wisely a model of `binary_module`; i.e. up to `funext`.
+definition is_model_ptwise (α β : Type _) [model binary_module α] [model binary_module β] : ∀ {n : ℕ} (r : binary_module.rel n) (var : finord n → hom α β) (a : α), ((binary_module.rel_lhs r).elim (@premodel.act binary_module (hom α β) _) var).val a = ((binary_module.rel_rhs r).elim (@premodel.act binary_module (hom α β) _) var).val a :=
   begin
     intros n r var a,
-    let hp := @optree.elim_funap binary_module.ops (finord n) (hom α β) β (@premodel.act binary_module (hom α β) _) (@premodel.act binary_module β _) var (hom_eval α β a).val (hom_eval α β a).property,
-    delta hom_eval at hp,
+    let hp := @optree.elim_funap binary_module.ops (finord n) (hom α β) β (@premodel.act binary_module (hom α β) _) (@premodel.act binary_module β _) var (hom.eval α β a).val (hom.eval α β a).property,
+    delta hom.eval at hp,
     dsimp [subtype.val,function.comp] at hp,
     rw [hp,hp],
     rw [model.axiom_eq],
   end
 
-#print axioms hom_model_funext
+#print axioms hom.is_model_ptwise
 
 namespace unsafe
 
 --- For every pair of `binary_module`s `α` and `β`, the type of homomorphisms `hom α β` is a model of `binary_module`.
-definition hom_model (α β : Type _) [model binary_module α] [model binary_module β] : model binary_module (hom α β) :=
+definition is_model (α β : Type _) [model binary_module α] [model binary_module β] : model binary_module (hom α β) :=
 {
-  haxiom := λ _ r var, subtype.eq (funext (@hom_model_funext α β _ _ _ r var))
+  haxiom := λ _ r var, subtype.eq (funext (@is_model_ptwise α β _ _ _ r var))
 }
 
-#print axioms hom_model
+#print axioms hom.unsafe.is_model
 
 end unsafe
+
+section
+
+local infixl `●`:100 := hom.comp
+local attribute [instance] binary_module.has_binary_add
+
+--- Composition is linear in the right operand.
+theorem comp_bilinear_right_ptwise (α β γ: Type _) [model binary_module α] [model binary_module β] [model binary_module γ] (g : hom β γ) (f₁ f₂ : hom α β) : ∀ (a : α), (g ● (f₁+ f₂)).val a = ((g ● f₁) + (g ● f₂)).val a :=
+  begin
+    intros a,
+    dsimp [has_add.add,binary_module.add,hom.comp,morphism.comp],
+    dsimp [premodel.act,vect.map,subtype.val],
+    csimp only [vect.unzip_fam_eval,vect.map],
+    rw [g.property],
+    dsimp [vect.map,function.comp],
+    refl
+  end
+
+#print axioms comp_bilinear_right_ptwise
+
+--- Composition is linear in the left operand.
+theorem comp_bilinear_left_ptwise (α β γ: Type _) [model binary_module α] [model binary_module β] [model binary_module γ] (g₁ g₂ : hom β γ) (f : hom α β) : ∀ (a : α), ((g₁+g₂) ● f).val a = ((g₁ ● f) + (g₂ ● f)).val a :=
+  begin
+    intros a,
+    dsimp [has_add.add,binary_module.add,hom.comp,morphism.comp],
+    dsimp [premodel.act,vect.map,subtype.val],
+    csimp only [vect.unzip_fam_eval,vect.map],
+  end
+
+#print axioms comp_bilinear_left_ptwise
+
+end
+
+end hom
 
 end binary_module
--- src/algebra/binary_module/prod.lean
+++ src/algebra/binary_module/prod.lean
@@ -1,4 +1,4 @@
-import .basic
+import .basic .hom
 
 /-!
  * Constructions and theorems on products of `binary_modules`.
@@ -6,14 +6,25 @@ import .basic
 
 namespace binary_module
 
+open binary_module
+
 local attribute [instance] has_binary_add
 attribute [instance] binary_module.has_trivial_init
+attribute [instance] model.prod
 
-definition prod_inl {α β : Type _} [model binary_module α] [model binary_module β] : morphism binary_module α (α×β) := @model.prod_inl binary_module binary_module.has_trivial_init α β _ _
+@[reducible,inline]
+definition prod_fst {α β : Type _} [model binary_module α] [model binary_module β] : hom (α×β) α := morphism.proj_fst binary_module
 
-definition prod_inr {α β : Type _} [model binary_module α] [model binary_module β] : morphism binary_module β (α×β) := @model.prod_inr binary_module binary_module.has_trivial_init α β _ _
+@[reducible,inline]
+definition prod_snd {α β : Type _} [model binary_module α] [model binary_module β] : hom (α×β) β := morphism.proj_snd binary_module
 
-lemma pair_is_sum {α β : Type _} [model binary_module α] [model binary_module β] : ∀ (x : α×β), x = (prod_inl x.fst) + (prod_inr x.snd) :=
+@[reducible,inline]
+definition prod_inl {α β : Type _} [model binary_module α] [model binary_module β] : hom α (α×β) := @model.prod_inl binary_module binary_module.has_trivial_init α β _ _
+
+@[reducible,inline]
+definition prod_inr {α β : Type _} [model binary_module α] [model binary_module β] : hom β (α×β) := @model.prod_inr binary_module binary_module.has_trivial_init α β _ _
+
+lemma pair_is_sum {α β : Type _} [model binary_module α] [model binary_module β] : ∀ (x : α×β), x = (prod_inl.val x.fst) + (prod_inr.val x.snd) :=
   begin
     intros x,
     cases x,
@@ -23,9 +34,6 @@ lemma pair_is_sum {α β : Type _} [model binary_module α] [model binary_module
     dsimp [premodel.act,prod.map],
     rw [vect.unzip_fst_is_map_fst,vect.unzip_snd_is_map_snd],
     dsimp [vect.map],
-    unfold coe_fn,
-    unfold has_coe_to_fun.coe,
-    dsimp *,
     repeat {rw [fixed_elem_is_zero]},
     let hfst := @binary_module.add_zero α _,
     let hsnd := @binary_module.zero_add β _,
@@ -36,9 +44,73 @@ lemma pair_is_sum {α β : Type _} [model binary_module α] [model binary_module
 #print axioms pair_is_sum
 
 
-local infixl `●`:100 := morphism.comp
+local infixl `●`:100 := hom.comp
 
-lemma co_unzip {α β γ : Type _} [model binary_module α] [model binary_module β] [model binary_module γ] (f : morphism binary_module (α×β) γ) : morphism binary_module α γ × morphism binary_module β γ :=
+--- Construct `hom (α×β) γ` from `hom α γ` and `hom β γ`.
+definition co_zip {α β γ : Type _} [model binary_module α] [model binary_module β] [model binary_module γ] (f : hom α γ) (g : hom β γ) : hom (α×β) γ := (f ● prod_fst) + (g ● prod_snd)
+
+--- Decompose `hom (α×β) γ` into `hom α γ` and `hom β γ`.
+definition co_unzip {α β γ : Type _} [model binary_module α] [model binary_module β] [model binary_module γ] (f : hom (α×β) γ) : hom α γ × hom β γ :=
   (f ● prod_inl, f ● prod_inr)
+
+--- `co_zip` after `co_unzip` recovers the original `hom (α×β) γ` pointwisely; i.e. the same as the original up to `funext`.
+theorem co_zip_of_co_unzip_ptwise {α β γ : Type _} [model binary_module α] [model binary_module β] [model binary_module γ] (f : hom (α×β) γ) : ∀ x, (co_zip (co_unzip f).fst (co_unzip f).snd).val x = f.val x :=
+  begin
+    intros x; cases x with a b,
+    dsimp [co_zip,co_unzip],
+    dsimp [has_add.add,binary_module.add],
+    dsimp [premodel.act],
+    rw [vect.unzip_fam_eval],
+    dsimp [vect.map],
+    dsimp [hom.comp,morphism.comp,function.comp],
+    let hf := λ x y, f.property ops.add ⁅x,y⁆,
+    dunfold vect.map at hf,
+    rw [←hf],
+    dsimp [prod_fst,morphism.proj_fst,prod_inl,model.prod_inl],
+    dsimp [prod_snd,morphism.proj_snd,prod_inr,model.prod_inr],
+    csimp only [fixed_elem_is_zero],
+    dsimp [premodel.act,prod.map,vect.unzip],
+    let hadd_zero := binary_module.add_zero a,
+    let hzero_add := binary_module.zero_add b,
+    dsimp [binary_module.zero,binary_module.add] at hadd_zero hzero_add,
+    rw [hadd_zero,hzero_add]
+  end
+
+#print axioms co_zip_of_co_unzip_ptwise
+
+theorem co_unzip_of_co_zip_ptwise {α β γ : Type _} [model binary_module α]  [model binary_module β] [model binary_module γ] (f : hom α γ) (g : hom β γ)  : (∀ a, (co_unzip (co_zip f g)).fst.val a = f.val a) ∧ (∀ b, (co_unzip (co_zip f g)).snd.val b = g.val b) :=
+  begin
+    split,
+    show ∀ (a : α), (co_unzip (co_zip f g)).fst.val a = f.val a, {
+      intros a,
+      dsimp [co_zip,co_unzip],
+      dsimp [hom.comp,morphism.comp,function.comp,subtype.val],
+      dsimp [prod_inl,model.prod_inl],
+      dsimp [vect.map],
+      dsimp [premodel.act,vect.unzip_fam],
+      dsimp [prod_fst,prod_snd, morphism.proj_fst, morphism.proj_snd],
+      rw [fixed_elem_is_zero,g.property],
+      dunfold vect.map,
+      let hz := add_zero (f.val a),
+      dsimp [binary_module.add,binary_module.zero] at hz,
+      rw [hz]
+    },
+    show ∀ (b : β), (co_unzip (co_zip f g)).snd.val b = g.val b, {
+      intros b,
+      dsimp [co_zip,co_unzip],
+      dsimp [hom.comp,morphism.comp,function.comp,subtype.val],
+      dsimp [prod_inr,model.prod_inr],
+      dsimp [vect.map],
+      dsimp [premodel.act,vect.unzip_fam],
+      dsimp [prod_fst,prod_snd, morphism.proj_fst, morphism.proj_snd],
+      rw [fixed_elem_is_zero,f.property],
+      dunfold vect.map,
+      let hz := zero_add (g.val b),
+      dsimp [binary_module.add,binary_module.zero] at hz,
+      rw [hz]
+    }
+  end
+
+#print axioms co_unzip_of_co_zip_ptwise
 
 end binary_module

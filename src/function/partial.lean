ec2507f1dcf55781f38d87be19e357c01ad1ba3f
Junology <jtachan@hotmail.com>
2021-09-17 13:46:15 +0900

    More on partial functions.

--- src/function/partial.lean
+++ src/function/partial.lean
@@ -26,6 +26,25 @@ instance defined_decidable {α β : Sort _} {f : partial α β} : decidable_pred
     (λ b h, is_true (λ hn, option.no_confusion (h.symm.trans hn)))
     (@rfl _ (f x))
 
+--- A partial function `f` is defined at a point `x` iff `f x = some b` for some `b`.
+lemma defined_iff_some {α β : Sort _} (f : partial α β) {x : α} : f.is_defined_at x ↔ ∃ b, f x = some b :=
+  begin
+    split,
+    show f.is_defined_at x → _, {
+      intros hx,
+      cases hfx : f x with b,
+      case none { exfalso; exact hx hfx },
+      exact ⟨b,rfl⟩
+    },
+    show _ → f.is_defined_at x, {
+      intros hsome; cases hsome with b hb,
+      intros h,
+      have : some b = none,
+        from hb.symm.trans h,
+      injection this
+    },
+  end
+
 --- Domain of partial functions.
 protected
 definition domain {α β : Sort _} (f : partial α β) : Sort _ :=
@@ -41,7 +60,7 @@ definition to_fun {α β : Sort _} (f : partial α β) : f.domain → β :=
 
 --- Specify how to determine the values of the function `f.to_fun` for a partial function `f`.
 protected
-lemma to_fun_value_of_eq {α β : Sort _} {f : partial α β} {x : f.domain} {y : β} : f x.val = some y → f.to_fun x = y :=
+lemma to_fun_value_of_eq {α β : Sort _} (f : partial α β) {x : f.domain} {y : β} : f x.val = some y → f.to_fun x = y :=
   begin
     intros hxy,
     dsimp [partial.to_fun],
@@ -53,12 +72,12 @@ lemma to_fun_value_of_eq {α β : Sort _} {f : partial α β} {x : f.domain} {y 
 
 --- For each partial function `f`, the values of `f` on its domain is determined by `f.to_fun`.
 protected
-lemma on_domain_is_to_fun {α β : Sort _} {f : partial α β} {x : f.domain} : f x.val = some (f.to_fun x) :=
+lemma to_fun_on_domain {α β : Sort _} {f : partial α β} {x : f.domain} : f x.val = some (f.to_fun x) :=
   begin
     cases hx : f x.val with y,
     case option.none { exact (x.property hx).elim },
     refine congr_arg some _,
-    exact (partial.to_fun_value_of_eq hx).symm
+    exact (f.to_fun_value_of_eq hx).symm
   end
 
 --- Think of an ordinary function as a partial function.
@@ -99,8 +118,8 @@ lemma injective_iff {α : Type _} {β : Sort _} (f : partial α β) : f.injectiv
       case option.none { exfalso; exact x.property hx },
       cases hy: f y.val with c,
       case option.none { exfalso; exact y.property hy },
-      rw [partial.to_fun_value_of_eq hx],
-      rw [partial.to_fun_value_of_eq hy],
+      rw [f.to_fun_value_of_eq hx],
+      rw [f.to_fun_value_of_eq hy],
       intros hbc,
       have hxy : f x.val = f y.val :=
         (hx.trans (congr_arg some hbc)).trans hy.symm,
@@ -115,13 +134,56 @@ lemma injective_iff {α : Type _} {β : Sort _} (f : partial α β) : f.injectiv
       have hbc : b = c,
         by apply option.some.inj; rw [←hfx,←hfy]; exact hfxy,
       have hfxv : f.to_fun ⟨x,hx⟩ = b,
-        from partial.to_fun_value_of_eq hfx,
+        from f.to_fun_value_of_eq hfx,
       have hfyv : f.to_fun ⟨y,hy⟩ = c,
-        from partial.to_fun_value_of_eq hfy,
+        from f.to_fun_value_of_eq hfy,
       let hxy := hinj ((hfxv.trans hbc).trans hfyv.symm),
       exact congr_arg subtype.val hxy,
     }
   end
+
+
+/-
+ * Partial function that is dual to the injection from a decidable subtypes.
+--/
+--- Dual of the injection from a decidable subtype
+definition coinj {α : Type _} (p : α → Prop) [decidable_pred p] : partial α (subtype p) :=
+  λ x, dite (p x) (λ h, some ⟨x,h⟩) (λ _, none)
+
+section coinj
+
+variables {α : Type _} {p : α → Prop} [decidable_pred p]
+
+--- A point belongs to the domain of `coinj p` if and only if it satisfies `p`.
+lemma coinj_domain : ∀ x, (coinj p).is_defined_at x ↔ p x :=
+  begin
+    intros x,
+    dsimp [coinj, partial.is_defined_at],
+    by_cases p x,
+    rw [dif_pos h]; calc
+      ¬some (subtype.mk x h) = none
+          ↔ true : iff_true_intro (λ h, option.no_confusion h)
+      ... ↔ p x : iff.symm (iff_true_intro h),
+    rw [dif_neg h]; calc
+      ¬none = none
+          ↔ false : ⟨false_of_ne,false.elim⟩
+      ... ↔ p x : iff.symm (iff_false_intro h)
+  end
+
+--- `coinj p` is injective as a partial function.
+lemma coinj_inj : (coinj p).injective :=
+  begin
+    intros x y hx hy hxy,
+    have hpx : p x,
+      from (coinj_domain x).mp hx,
+    have hpy : p y,
+      from (coinj_domain y).mp hy,
+    dsimp [coinj] at hxy,
+    rw [dif_pos hpx, dif_pos hpy] at hxy,
+    exact congr_arg subtype.val (option.some.inj hxy)
+  end
+
+end coinj
 
 end partial
 
